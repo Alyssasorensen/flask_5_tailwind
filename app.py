ee10b8f1dda293c4270550bb0ba09f594254f3a0
Alyssasorensen <-m>
2023-10-31 03:10:06 +0000

    updated app.py and index_tailwind.html files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Retrieve data from the database
+    return render_template('index_tailwind.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
